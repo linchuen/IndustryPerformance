0e64202a90e081a5d4f9f6b624b76758fb7e7353
Aiden <x29587629@gmail.com>
2022-02-19 14:19:30 +0800

    init

---
+++ src/main/java/cooba/IndustryPerformance/IndustryPerformanceApplication.java
@@ -0,0 +1,13 @@
+package cooba.IndustryPerformance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndustryPerformanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndustryPerformanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cooba/IndustryPerformance/IndustryPerformanceApplicationTests.java
@@ -0,0 +1,13 @@
+package cooba.IndustryPerformance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndustryPerformanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
