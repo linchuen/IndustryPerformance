4509c2308a1dcaa714abde4583f67b971c199542
Aiden <x29587629@gmail.com>
2022-03-06 18:03:01 +0800

    修正bug

--- src/test/java/cooba/IndustryPerformance/industryTest.java
+++ src/test/java/cooba/IndustryPerformance/industryTest.java
@@ -1,6 +1,5 @@
 package cooba.IndustryPerformance;
 
-import cooba.IndustryPerformance.constant.RedisConstant;
 import cooba.IndustryPerformance.constant.UrlEnum;
 import cooba.IndustryPerformance.service.IndustryService;
 import cooba.IndustryPerformance.service.StockService;
@@ -26,20 +25,26 @@ public class industryTest {
     RedisTemplate redisTemplate;
 
     @Test
+    public void buildIndustryInfoTest() {
+        industryService.biuldAllIndustryInfo();
+        //industryService.buildIndustryInfo(UrlEnum.金融.name());
+    }
+
+    @Test
     public void getIndustryStockInfoTest() throws InterruptedException {
         StopWatch stopWatch = new StopWatch("IndustryStockInfo");
         //redisTemplate.delete(RedisConstant.INDUSTRYINFO + UrlEnum.交通航運.name());
         stopWatch.start("多執行緒測試");
-        for (int i=0;i<50;i++){
-            CompletableFuture.supplyAsync(() -> industryService.getIndustryStockInfo(UrlEnum.交通航運.name()), Executors.newFixedThreadPool(5));
-            CompletableFuture.supplyAsync(() -> industryService.getIndustryStockInfo(UrlEnum.交通航運.name()), Executors.newFixedThreadPool(5));
+        for (int i = 0; i < 50; i++) {
+            CompletableFuture.supplyAsync(() -> industryService.getIndustryStockInfo(UrlEnum.金融.name()), Executors.newFixedThreadPool(5));
+            CompletableFuture.supplyAsync(() -> industryService.getIndustryStockInfo(UrlEnum.金融.name()), Executors.newFixedThreadPool(5));
             Thread.sleep(1);
         }
         stopWatch.stop();
         //redisTemplate.delete(RedisConstant.INDUSTRYINFO + UrlEnum.交通航運.name());
         stopWatch.start("單執行緒測試");
-        for (int i=0;i<100;i++){
-            industryService.getIndustryStockInfo(UrlEnum.交通航運.name());
+        for (int i = 0; i < 100; i++) {
+            industryService.getIndustryStockInfo(UrlEnum.金融.name());
         }
         stopWatch.stop();
         Thread.sleep(1000);
@@ -51,7 +56,7 @@ public class industryTest {
         //stockService.deleteAllStockDetail();
         StopWatch stopWatch = new StopWatch("Async");
         stopWatch.start();
-        industryService.buildIndustryStockDetailInfo(UrlEnum.醫療器材.name());
+        CompletableFuture.runAsync(() -> industryService.buildIndustryStockDetailInfo(UrlEnum.金融.name()));
         stopWatch.stop();
         log.info("Async 耗時:{}", stopWatch.getTotalTimeSeconds());
     }
@@ -62,7 +67,7 @@ public class industryTest {
         //stockService.deleteAllStockDetail();
         StopWatch stopWatch = new StopWatch("WithoutAsync");
         stopWatch.start();
-        Map<String, String> industryStockMap = industryService.getIndustryStockInfo(UrlEnum.醫療器材.name());
+        Map<String, String> industryStockMap = industryService.getIndustryStockInfo(UrlEnum.金融.name());
         if (industryStockMap.isEmpty()) {
             log.warn("industryStockMap 為空");
             return;
@@ -73,9 +78,8 @@ public class industryTest {
     }
 
     @Test
-    public void buildIndustryInfoTest() {
-        //industryService.biuldAllIndustryInfo();
-        industryService.buildIndustryInfo(UrlEnum.金融.name());
+    public void getSubIndustryInfoTest() {
+        System.out.println(industryService.getSubIndustryInfo(UrlEnum.金融.name()));
     }
 
     @Test
@@ -85,6 +89,6 @@ public class industryTest {
 
     @Test
     public void getSubIndustryGrowthTest() {
-        industryService.getSubIndustryInfo(UrlEnum.金融.name()).forEach(s -> industryService.getSubIndustryGrowth(UrlEnum.金融.name(),s));
+        industryService.getSubIndustryInfo(UrlEnum.金融.name()).forEach(s -> industryService.getSubIndustryGrowth(UrlEnum.金融.name(), s));
     }
 }
--- src/test/java/cooba/IndustryPerformance/stockTest.java
+++ src/test/java/cooba/IndustryPerformance/stockTest.java
@@ -3,8 +3,9 @@ package cooba.IndustryPerformance;
 import cooba.IndustryPerformance.service.StockService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 public class stockTest {
     @Autowired
     private StockService stockService;
